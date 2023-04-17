a83a8bd5de6391a108d0e08d3ef34b5d69463a4c
김민서 <a01024507607@gmail.com>
2023-04-17 09:32:44 +0900

    'mongodbPage'

---
+++ app/mongodb/page.js
@@ -0,0 +1,13 @@
+import { connectDB } from "@/util/database.js";
+import { MongoClient } from "mongodb"
+
+export default async function() {
+    const db = (await connectDB).db("aibook")
+    let result = await db.collection('list').find().toArray()
+
+    return(
+        <div>
+
+        </div>
+    )
+}
