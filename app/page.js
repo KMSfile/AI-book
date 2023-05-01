955876d141537707353509fed6eaf673e3515421
김민서 <a01024507607@gmail.com>
2023-05-01 10:15:43 +0900

    'button1'

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
 import Link from "next/link"
+import Image from "next/image"
+import rendombook from '/public/img/rendombook.png'
 
 export default async function Home() {
 
   return (
     <div className="container">
 
-      <div className="box1"></div>
+      <div className="box1">
+        <h2>랜덤북</h2>
+        <p>주제부터 내용까지 모두 AI에게 맡겨보세요! 재밌는 이야기를 써줄거에요!</p>
+        <Image className="backimage" src={ rendombook } width={250}/>
+        <button className="pagebutton">만들러가기</button>
+      </div>
+
       <div className="box2"></div>
       <div className="box3"></div>
 
